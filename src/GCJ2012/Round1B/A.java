5906221b230ec49bc10f4df96b153cc1bed2df09
Zhenyi Luo <zhenyiluo@gmail.com>
2015-04-30 21:01:12 -0400

    modify incorrect solution

--- src/GCJ2012/Round1B/A.java
+++ src/GCJ2012/Round1B/A.java
@@ -3,96 +3,61 @@ package GCJ2012.Round1B;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 /**
  * Created by Zhenyi Luo on 15-4-30.
  */
 public class A {
-    final static String PROBLEM_NAME = "A-small-practice";
+    final static String PROBLEM_NAME = "A-large-practice";
     final static String WORK_DIR = "/Users/cecilia/Downloads/" + PROBLEM_NAME;
 
-    public static class Contestant{
-        int id;
-        int value;
-        boolean flag = true;
-        double perc = 0;
-        public Contestant(int id, int value){
-            this.id = id;
-            this.value = value;
-        }
-    }
-    public void solve(Scanner sc, PrintWriter pw){
+    public void solve(Scanner sc, PrintWriter pw) {
         int n = sc.nextInt();
-        ArrayList<Contestant> list = new ArrayList<>();
+        int[] a = new int[n];
         int sum = 0;
-        boolean flag = true;
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            int num = sc.nextInt();
-            if(num == 0){
-                flag = false;
-                count++;
-            }
-            sum += num;
-            list.add(new Contestant(i, num));
-        }
-        if(!flag){
-            for(Contestant contestant : list){
-                if(contestant.value!= 0){
-                    contestant.perc = 0;
-                }else{
-                    contestant.perc = 100.0 / count;
-                }
-                pw.print(contestant.perc + " ");
-            }
-            pw.println();
-            return;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            sum += a[i];
         }
 
-        Collections.sort(list, new Comparator<Contestant>(){
-           @Override
-            public int compare(Contestant c1, Contestant c2){
-               return c2.value - c1.value;
-           }
-        });
-        for(Contestant contestant : list){
-            if(1.0 * contestant.value >= (2.0 * sum / n)){
-                contestant.flag = false;
-                n--;
-                sum -= contestant.value;
-            }else{
-                break;
+        for (int i = 0; i < n; i++) {
+            double l = 0;
+            double h = 1;
+            double mid = 0;
+            while (h - l > 0.0000000001) {
+                mid = (l + h) / 2;
+                if (check(i, mid, sum, a, n)) {
+                    l = mid;
+                } else {
+                    h = mid;
+                }
             }
-        }
-        for(Contestant contestant : list){
-            if(contestant.flag){
-                contestant.perc = ((2.0 * sum / n) - contestant.value) / sum;
-            }
-        }
-        Collections.sort(list, new Comparator<Contestant>(){
-            @Override
-            public int compare(Contestant c1, Contestant c2){
-                return c1.id - c2.id;
-            }
-        });
-        for(Contestant contestant : list){
-            pw.print(contestant.perc + " ");
+            pw.printf("%.6f", mid * 100);
+            pw.print(" ");
         }
         pw.println();
     }
 
+    public boolean check(int i, double p, int sum, int[] a, int n) {
+        double sumP = 0;
+        double v = a[i] + p * sum;
+        for (int k = 0; k < n; k++) {
+            if (k == i) {
+                continue;
+            }
+            sumP += Math.max(0, (v - a[k]) / sum);
+        }
+        return sumP <= 1 - p;
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new FileReader(WORK_DIR + ".in"));
         PrintWriter pw = new PrintWriter(new FileWriter(WORK_DIR + ".out"));
         int caseCnt = sc.nextInt();
-        for (int caseNum=0; caseNum<caseCnt; caseNum++) {
+        for (int caseNum = 0; caseNum < caseCnt; caseNum++) {
             System.out.println("Processing test case " + (caseNum + 1));
-            pw.print("Case #" + (caseNum+1) + ": ");
+            pw.print("Case #" + (caseNum + 1) + ": ");
             new A().solve(sc, pw);
         }
         pw.flush();
